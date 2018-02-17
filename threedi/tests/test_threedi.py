08b224e30c39de3d5b36abdb2cb1571dc86fed6c
Jackie Leng <jccleng@gmail.com>
2018-02-17 23:38:37 +0100

    more tests

--- threedi/tests/test_threedi.py
+++ threedi/tests/test_threedi.py
@@ -5,11 +5,18 @@ from threedi import (
 api = API()
 
 
-def test_smoke():
-    assert isinstance(api.foo, API)
+def test_api_traversal():
     assert isinstance(api.foo.bar, API)
+    assert api.foo.bar._cache == ['foo', 'bar']
 
 
-def test_http_method():
-    assert not isinstance(api.foo.get, API)
-    assert not isinstance(api.foo.post, API)
+def test_using_reserved_python_keywords():
+    endpoint = api.foo._('for').bar
+    assert isinstance(endpoint, API)
+    assert endpoint._cache == ['foo', 'for', 'bar']
+
+
+def test_http_methods():
+    assert not callable(api.foo)  # smoke test
+    assert callable(api.foo.get)
+    assert callable(api.foo.post)
