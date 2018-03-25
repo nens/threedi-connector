335c4a241852202a0f2eada39fcef2f896282b69
Jackie Leng <jccleng@gmail.com>
2018-03-25 01:40:31 +0100

    Add doc for get_credentials_interactively

--- threedi_connector/helpers.py
+++ threedi_connector/helpers.py
@@ -7,6 +7,7 @@ Credentials = namedtuple('Credentials', ['username', 'password'])
 
 
 def get_credentials_interactively():
+    """Get username and password via stdin."""
     username = raw_input("Username: ")
     password = getpass.getpass("Password: ")
     return Credentials(username, password)
